ad746f3a6b5b5bd6e67558190aa8fe2f3d8755cc
singcl <24661881@qq.com>
2017-12-29 12:45:45 +0800

    配置karma测试用例

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function reverse(str) {
+    return str.split('').reverse().join('');
+}
+
+function isInteger(num) {
+    if (typeof num !== "number") return false;
+    var pattern = /^[1-9]\d*$/g;
+    return pattern.test(num);
+}
